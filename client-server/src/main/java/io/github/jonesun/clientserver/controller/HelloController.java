b8bd515bdb1e0ddbbcbb5f9af109dc10bc1ca341
jone.sun <sunr922@163.com>
2021-01-20 11:41:33 +0800

    spring boot 2.4.2存在中文乱码问题回退到2.4.1即可

--- client-server/src/main/java/io/github/jonesun/clientserver/controller/HelloController.java
+++ client-server/src/main/java/io/github/jonesun/clientserver/controller/HelloController.java
@@ -1,5 +1,7 @@
 package io.github.jonesun.clientserver.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
     @GetMapping
     public String index() {
+        logger.info("这是一个样例");
+        logger.error("这是一个样例");
+        System.out.println("这是一个样例");
         return "Hello World";
     }
 
